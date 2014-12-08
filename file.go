3901d0a282dab2741187cdac025baafed2283247
Andrew Martin <a.a.d.martin@gmail.com>
2014-12-08 15:31:25 +1100

    Added case for syscall types

--- file.go
+++ file.go
@@ -29,6 +29,36 @@ func main() {
   cmd.Start()
   r := bufio.NewReader(stdout)
 
+  for {
+
+      bufline, err := r.ReadString('\n')
+      line := string(bufline)
+
+      lines := strings.Split(line, " ")
+      filestring := strings.Split(lines[3], `"`)
+
+      switch {
+
+          case strings.HasPrefix(lines[3], "open"):
+              log.Print("open ", filestring[1])
+
+          case strings.HasPrefix(lines[3], "stat"):
+              log.Print("stat ", filestring[1])
+
+          case strings.HasPrefix(lines[3], "readlink"):
+              log.Print("check symlink ", filestring[1])
+
+
+
+          default:
+             log.Print(lines[3])
+
+      }
+
+      if err != nil {
+         log.Fatal(err)
+      }
+  }
 
 
 
