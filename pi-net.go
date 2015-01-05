d2347ad4a47b9a509c550d71edc082e4c87a23e3
Andrew Martin <a.a.d.martin@gmail.com>
2015-01-05 16:00:28 +1100

    Added net syscall

---
+++ pi-net.go
@@ -0,0 +1,62 @@
+
+package main
+
+import (
+        "os/exec"
+        "bufio"
+        "log"
+        "strings"
+        "flag"
+        )
+
+
+var pid string
+
+func init() {
+
+    flag.StringVar(&pid, "p", "1", "Pid")
+    flag.Parse()
+
+}
+
+
+func main() {
+
+  cmd := exec.Command("strace", "-f", "-e", "trace=network", "-p", pid)
+  stdout, _ := cmd.StderrPipe()
+  cmd.Start()
+  r := bufio.NewReader(stdout)
+
+  for {
+
+      bufline, err := r.ReadString('\n')
+      line := string(bufline)
+
+      
+      lines := strings.Split(line, " ")
+      if len(lines) > 3 {
+      log.Print(lines[3])
+
+      switch {
+
+          case strings.HasPrefix(lines[3], "recvfrom"):
+
+          //default:
+          //   log.Print("Undefined :",line, "    ",filestring)
+
+      }
+
+      }
+
+      if err != nil {
+         log.Fatal(err)
+      }
+  }
+
+
+
+}
+
+
+
+
