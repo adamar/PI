28a1a9622fb5b455c5fc70862b390d7ef42351d3
Andrew Martin <a.a.d.martin@gmail.com>
2015-04-17 12:53:49 +1000

    Added mem grepper

---
+++ pi-mem-grep.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+type Command struct {
+	Name string
+	Args []string
+	Grep string
+}
+
+type Result struct {
+	Output []string
+}
+
+func (c *Command) execCommand() error {
+
+	bin, err := exec.LookPath(c.Name)
+	if err != nil {
+		return err
+	}
+
+	output, err := exec.Command(bin, c.Args...).CombinedOutput()
+	if err != nil {
+		return err
+	}
+
+	log.Print(string(output))
+
+	return nil
+
+}
+
+func buildCommand(command []string, grep string) *Command {
+	return &Command{Name: command[0], Args: command[1:], Grep: grep}
+}
+
+func main() {
+
+	arr := []string{"gdp", "--batch", "-pid", "30182", "-ex", `"dump memory 0x7fff145c4000 0x7fff145e5000"`}
+	ex := buildCommand(arr, "grepstring")
+	ex.execCommand()
+
+}
