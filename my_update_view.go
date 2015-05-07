9c3f072975fa73117b9e277e2fdb98961c3afdf9
Andrew Martin <a.a.d.martin@gmail.com>
2015-05-08 09:35:55 +1000

    Added remaining functions for GUI

--- my_update_view.go
+++ my_update_view.go
@@ -60,3 +60,77 @@ func (pi *PI) Start() {
 	}
 
 }
+
+func (pi *PI) run() {
+
+	ch := make(chan string)
+
+	go func(ch chan string) {
+		cmd := exec.Command("sudo", "strace", "-f", "-e", "trace=network", "-p", "14295")
+		stdout, _ := cmd.StderrPipe()
+		cmd.Start()
+		r := bufio.NewReader(stdout)
+
+		for {
+
+			bufline, _ := r.ReadString('\n')
+			ch <- bufline
+		}
+
+	}(ch)
+
+	for {
+		data := <-ch
+		if _, ok := pi.Syscalls[data]; ok {
+			pi.Syscalls[data] = pi.Syscalls[data] + 1
+		} else {
+			pi.Syscalls[data] = 1
+		}
+		//pi.Syscalls = append(pi.Syscalls, data)
+		pi.Current = data
+	}
+
+}
+
+func (pi *PI) updateView() {
+	for {
+		time.Sleep(time.Second)
+		v, err := pi.GUI.View("center")
+		if err != nil {
+			panic(err)
+		}
+		v.Clear()
+		pi.display(v)
+		pi.GUI.Flush()
+	}
+}
+
+func (pi *PI) display(v io.Writer) error {
+
+	output := ""
+
+	//sort.Strings(pi.Syscalls)
+
+	for k, _ := range pi.Syscalls {
+		if k == pi.Current {
+			output += fmt.Sprintf("=>" + k)
+		} else {
+			output += fmt.Sprintf(k)
+		}
+	}
+
+	fmt.Fprintf(v, output)
+	return nil
+}
+
+func (pi *PI) layout(*gocui.Gui) error {
+	maxX, maxY := pi.GUI.Size()
+	if v, err := pi.GUI.SetView("center", 3, 0, maxX, maxY); err != nil {
+		if err != gocui.ErrorUnkView {
+			return err
+		}
+		v.Frame = false
+		pi.display(v)
+	}
+	return nil
+}
