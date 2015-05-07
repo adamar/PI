0073c022bfa7f53c4614b78f019e7872444857a4
Andrew Martin <a.a.d.martin@gmail.com>
2015-05-08 09:35:21 +1000

    Added gui start and instantiation

--- my_update_view.go
+++ my_update_view.go
@@ -24,3 +24,39 @@ func main() {
 	pi.Start()
 
 }
+
+// NewPI returns a new PI instance
+func NewPI() *PI {
+
+	pi := PI{}
+
+	var err error
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		panic(err)
+	}
+
+	g.SetLayout(pi.layout)
+	if err = g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, func(g *gocui.Gui, v *gocui.View) error {
+		panic(err)
+	}); err != nil {
+		log.Panicln(err)
+	}
+
+	pi.Syscalls = map[string]int{}
+	pi.GUI = g
+	return &pi
+
+}
+
+func (pi *PI) Start() {
+	defer pi.GUI.Close()
+
+	go pi.run()
+	go pi.updateView()
+	err := pi.GUI.MainLoop()
+	if err != nil {
+		panic(err)
+	}
+
+}
